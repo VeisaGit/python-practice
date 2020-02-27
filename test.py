9e0226cd8c1601d5c69151544ca64de8e583a6f2
Veisa <veisa@veisa.me>
2020-02-27 21:30:18 +1000

    learning about functions

---
+++ test.py
@@ -0,0 +1,13 @@
+
+
+
+def simple (subject, action="washing", object="window"):
+    print("who - ", subject)
+    print("doing - ", action)
+    print("with - ", object)
+    print("And then: ", subject, action, object)
+
+
+simple(subject="Mom")
+
+#4.5 Произвольное число параметров
