828bf6bbedf61a7b97bdf8e3a226eca844881fb7
Veisa <veisa@veisa.me>
2020-03-05 23:11:56 +1000

    Fix problem, when sometimes in dict was 3 or 2 numbers instead 4

--- Cows and Bulls game/server.py
+++ Cows and Bulls game/server.py
@@ -2,26 +2,23 @@ import random
 
 
 number = str(random.randint(1000, 9999))
+
 print(number, ' - загаданное число')
 
 def check_number(u_number):
 
-    r = dict(zip(number, u_number))
-    print(r)
-
     cows = []
     bulls = []
 
-    for i in r:
-        if i == r[i]:
+    for i, j in zip(number, u_number):
+        if i == j:
             print(i)
             bulls.append(1)
         else:
             print(i)
             cows.append(1)
 
+
     print("Коровы:{} Быки: {}".format(sum(cows), sum(bulls)))
 
-
-    # print(u_number, ' - введенное число')
 
