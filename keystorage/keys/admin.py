276d649655cf5f68d04811914c9f8f66a4968680
Veisa <veisa@veisa.me>
2020-03-26 20:55:50 +1000

    Add database and admin panel

--- keystorage/keys/admin.py
+++ keystorage/keys/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from . models import Keys
+
+
 
 # Register your models here.
+
+
+admin.site.register(Keys)
+
+
--- keystorage/keys/views.py
+++ keystorage/keys/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from . models import Keys
 
 # Create your views here.
 
 def hello(request):
-    return render(request, 'keys/index.html')
+    data = {
+        'data': Keys.objects.all()
+
+    }
+    return render(request, 'keys/index.html', data)
+
