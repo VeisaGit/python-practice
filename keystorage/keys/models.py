ab59e4cc68508a64e6b304d79c3810141bb08254
Veisa <veisa@veisa.me>
2020-03-26 22:39:40 +1000

    Add bostrapp and luttle design

--- keystorage/keys/models.py
+++ keystorage/keys/models.py
@@ -1,16 +1,16 @@
 from django.db import models
 from multiselectfield import MultiSelectField
 
-# Create your models here.
-
 
 class Keys(models.Model):
-    key = models.CharField(max_length=30)
+    key = models.CharField(max_length=250)
     key_user = models.CharField(max_length=10)
     key_kcv = models.CharField(max_length=7)
     key_type = models.CharField(max_length=3)
     test_prod_choice = (
         ('test', 'test'),
-        ('prod', 'prod')
+        ('prod', 'prod'),
     )
-    choice = MultiSelectField(choices= test_prod_choice)
+    choice = MultiSelectField(choices=test_prod_choice)
+
+
