e30a44d28e38f4ee41f61246ae1b9b73fe19f8e2
Veisa <veisa@veisa.me>
2020-03-31 18:48:18 +1000

    First work version

--- TelegramBot/Tbot.py
+++ TelegramBot/Tbot.py
@@ -1,20 +1,8 @@
-from telegram import Update
+from telegram import Update, ParseMode
 from telegram.ext import Updater, CallbackContext
 from telegram.ext import Filters, MessageHandler
-from .Commands import *
+from Commands import *
 
-
-def message_handler(update: Update, context: CallbackContext):
-    text = update.message.text
-    if text == '110/70':
-        update.message.reply_text(
-            text="Спасибо, запись добавлена в дневник!")
-    elif text == 'Инструкции':
-        update.message.reply_text(
-            text="Чтобы фиксировать артериальное давление в дневник, нужно вводить его в определенном формате - \"Систолическое Дитолическое\" через пробел. Например: 110 70. Также бот может отправлять вам статистику за день, неделю, месяц сюда или на эл. почту.")
-    else:
-        update.message.reply_text(
-            text="Привет, я бот, которой поможет тебе вести дневник измерений артериального давления. Еще я могу возвращать статистику. Введите \"Инструкции\", чтобы понять, как заносить данные в дневник. Введите \"Команды\", чтобы узнать, что еще я умею.")
 
 def main():
     print('Start')
@@ -24,8 +12,19 @@ def main():
     )
     updater.dispatcher.add_handler(MessageHandler(filters=Filters.all, callback=message_handler))
 
-    updater.start_polling()
+    updater.start_polling() #делает работу бота постоянной
     updater.idle()
 
-if __name__ =='__main__':
+def message_handler(update: Update, context: CallbackContext):
+    text = update.message.text
+
+    if text:
+        update.message.reply_text(check_user_message(text), parse_mode=ParseMode.HTML)
+    elif text == 'Инструкции':
+        update.message.reply_text(instructions(text), parse_mode=ParseMode.HTML)
+    else:
+        update.message.reply_text(alternative_message(text), parse_mode=ParseMode.HTML)
+
+
+if __name__ == '__main__':
     main()
