c38ec7859385ba9ed68a435cf43f9c866da78b1e
Veisa <veisa@veisa.me>
2020-03-31 23:04:31 +1000

    Add DB sqlite and import messages to it

--- TelegramBot/Tbot.py
+++ TelegramBot/Tbot.py
@@ -2,24 +2,33 @@ from telegram import Update, ParseMode
 from telegram.ext import Updater, CallbackContext
 from telegram.ext import Filters, MessageHandler
 from Commands import *
+from db import *
 
 
 def main():
-    print('Start')
+    print('start')
     updater = Updater(
         token='1093115241:AAHC4pU5yX1w9jhPRkcXEqv9Wy_REo8C8Mc',
         use_context=True,
     )
+
     updater.dispatcher.add_handler(MessageHandler(filters=Filters.all, callback=message_handler))
 
     updater.start_polling() #делает работу бота постоянной
     updater.idle()
 
-def message_handler(update: Update, context: CallbackContext):
-    text = update.message.text
 
-    if text:
-        update.message.reply_text(check_user_message(text), parse_mode=ParseMode.HTML)
+def message_handler(update: Update, context: CallbackContext):
+    text = update.effective_message.text
+    user = update.effective_user
+
+    if text == '110 70':
+        update.message.reply_text(check_user_message(text,user), parse_mode=ParseMode.HTML)
+
+        add_message(
+            user_id=user.id,
+            text=text,)
+
     elif text == 'Инструкции':
         update.message.reply_text(instructions(text), parse_mode=ParseMode.HTML)
     else:
